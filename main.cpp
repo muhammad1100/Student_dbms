013bacf83afb39a0aad1758d82d24a14336c71b8
Asnahasan90 <asnajawad1527@gmail.com>
2025-01-26 13:23:51 +0500

    done by asna

--- main.cpp
+++ main.cpp
@@ -113,4 +113,58 @@ void view_Specific(string id) {
                  << setw(10) << s.eng << setw(8) << s.gradeEng << 
                  setw(10) << s.phy << setw(8) << s.gradephy
                  << setw(10) << s.cal << setw(8) << s.gradecal << endl<<endl;
-                 
+                 found = true;
+            break;
+        }
+    }
+     cout<<"\t\t\t\t============================================="<<endl;
+
+    if (!found) {
+        cout << "Record not found for ID: " << id << endl;
+    }
+
+    infile.close();
+}
+
+// Modify a student record
+void modify(string id) {
+    ifstream infile("Database.txt");
+    ofstream tempFile("TempDatabase.txt");
+    if (!infile || !tempFile) {
+        cerr << "Error: Could not open file.\n";
+        return;
+    }
+
+    student s;
+    bool found = false;
+
+    while (infile >> s.firstname >> s.secondname >> s.id >> s.pf >> s.gradepf >> s.ict >> s.gradeict >> s.eng >> s.gradeEng >> s.phy >> s.gradephy >> s.cal >> s.gradecal) {
+        if (s.id == id) {
+            found = true;
+            cout<<"\t\t\t\t========UPDATE STUDENT DATA==========\n\n";
+            cout <<"\t\t\t\tModifying record for ID: " << id << endl;
+            cout <<"\t\t\t\tEnter new First Name: ";
+            cin >> s.firstname;
+            cout <<"\t\t\t\tEnter new Second Name: ";
+            cin >> s.secondname;
+
+            cout <<"\t\t\t\tEnter new Programming Fundamentals Marks: ";
+            cin >> s.pf;
+            s.gradepf = calculateGrade(s.pf);
+
+            cout <<"\t\t\t\tEnter new ICT Marks: ";
+            cin >> s.ict;
+            s.gradeict = calculateGrade(s.ict);
+
+            cout <<"\t\t\t\tEnter new English Marks: ";
+            cin >> s.eng;
+            s.gradeEng = calculateGrade(s.eng);
+
+            cout <<"\t\t\t\tEnter new Physics Marks: ";
+            cin >> s.phy;
+            s.gradephy = calculateGrade(s.phy);
+
+            cout <<"\t\t\t\tEnter new Calculus Marks: ";
+            cin >> s.cal;
+            s.gradecal = calculateGrade(s.cal);
+        }
