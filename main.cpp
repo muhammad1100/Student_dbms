43b4953f29995a76fa897d3873805bd1a73cdf06
muhammad1100 <mb6472110@gmail.com>
2025-01-25 16:08:20 -0800

    done by bilal

--- main.cpp
+++ main.cpp
@@ -76,3 +76,14 @@ void intro() {
     cout << endl;
 }
 //introduction
+
+// Calculate grades based on marks
+string calculateGrade(float marks) {
+    if (marks >= 90) return "A+";
+    else if (marks >= 80) return "A";
+    else if (marks >= 70) return "B";
+    else if (marks >= 60) return "C";
+    else if (marks >= 50) return "D";
+    else return "F";
+}
+//Calculated grades
